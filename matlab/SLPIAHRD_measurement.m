e7750f598337d7dc292893b186ff4413e82fcfd2
Peter Polcz <ppolcz@gmail.com>
2024-02-26 13:02:19 +0100

    Old matlab files removed

--- matlab/SLPIAHRD_measurement.m
+++
@@ -1,6 +0,0 @@
-function [yk] = SLPIAHRD_measurement(xk)
-%SEIR_MEASUREMENT Summary of this function goes here
-%   Detailed explanation goes here
-yk = xk(6);
-end
-
--- matlab/figgen.m
+++
@@ -1,5 +0,0 @@
-close all;
-clc;
-
-%load('result/result_09-01-23_21:16.mat');
-
