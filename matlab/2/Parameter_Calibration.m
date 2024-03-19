d4e01c8464ed7ca0b20c76c700801cf7589aa006
Peter Polcz <ppolcz@gmail.com>
2024-03-19 12:41:39 +0100

    Minimalis

--- matlab/2/Parameter_Calibration.m
+++ matlab/2/Parameter_Calibration.m
@@ -9,10 +9,12 @@
 % Population in the simulator
 Np = C.Np;
 
-RESULT = "Result_2024-02-13_16-59_T28_allcomb";
-xls = "/home/ppolcz/Dropbox/Peti/Munka/01_PPKE_2020/PanSim_Results_2/" + RESULT + "/A47.xls";
-xls = "/home/ppolcz/Dropbox/Peti/Munka/01_PPKE_2020/PanSim_Results_2/GenLUT/Fig_2024-02-26_14-28.xls";
-R = readtimetable(xls);
+xlsnames = dir('/home/ppolcz/Dropbox/Peti/NagyGep/PanSim_Output/Allcomb_2024-03-07/*.xls');
+idx = randperm(numel(xlsnames),1);
+idx = 323;
+xls = xlsnames(idx);
+
+R = readtimetable(fullfile(xls.folder,xls.name));
 R.Date.Format = 'uuuu-MM-dd';
 
 Start_Date = datetime(2020,10,01);
@@ -21,7 +23,7 @@ End_Date = datetime(2021,01,31);
 
 R = R(isbetween(R.Date,Start_Date,End_Date),:);
 
-%%
+%%%
 
 fp = pcz_mfilename(mfilename('fullpath'));
 Q = readtable(string(fp.dir) + "/Parameters/Par_HUN_2023-12-19_JN1.xlsx", ...
@@ -33,12 +35,23 @@ Q = Q(["Transient","Original","Future"],:);
 % Q("Original","Period_A") = table(7);
 % Q("Original","Pr_D") = table(0.48);
 
+% Itt szandekosan elrontom a parametereket
+Q("Original","Pr_I") = table(0.7);
+Q("Original","Period_L") = table(5.5);
+Q("Original","Period_P") = table(10.1);
+Q("Original","Period_A") = table(10);
+Q("Original","Pr_D") = table(0.1);
+
+
+Q("Original","Period_L") = table(1.8);
+
+
 % 2024.02.26. (február 26, hétfő), 15:06
 Q("Original","Pr_I") = table(0.46);
-Q("Original","Period_L") = table(1.5);
-Q("Original","Period_P") = table(3.1);
-Q("Original","Period_A") = table(4);
-Q("Original","Pr_D") = table(0.48);
+% Q("Original","Period_L") = table(1.5);
+% Q("Original","Period_P") = table(3.1);
+% Q("Original","Period_A") = table(4);
+% Q("Original","Pr_D") = table(1.2);
 
 
 P = Epid_Par.Get(Q);
@@ -47,8 +60,6 @@ P = hp.param2table(P.Param);
 R(:,P.Properties.VariableNames) = P;
 
 K = Epid_Par.GetK;
-
-%%
 
 R = rec_SLPIAHDR(R,"Visualize",true);
 
